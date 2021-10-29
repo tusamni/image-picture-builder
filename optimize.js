d7e9f4400a3625bda6f67f1379b60a3b7b1b2499
Erik Olsen <erik@eriksolsen.com>
2021-10-28 19:50:41 -0500

    commit

--- optimize.js
+++ optimize.js
@@ -1,44 +1,42 @@
-const { map } = require('bluebird');
 const fs = require('fs');
 const path = require('path');
 const sharp = require('sharp');
+const exifr = require('exifr');
 
 let input = './input/';
 let output = './output/'
 
-let sizes = [2000, 1440, 1080, 720, 480];
+let sizes = [2000];
 let formats = ['jpeg', 'webp', 'avif'];
-
-async function optimizeImage(image) {
-    let extension = path.extname(image); // extension of the image file
-    let baseFilename = path.basename(image, extension); // filename of the image minus the extension
-    let outputPath = output + baseFilename;
-
-    sizes.map(function (size) {
-        formats.map(function (format) {
-            sharp(`${input}${image}`)
-                .toFormat(format)
-                .resize(size, size, { fit: "inside" })
-                .toFile(`${outputPath}-w${size}.${format}`)
-                .then(info => { console.log(info) })
-                .catch(err => { console.log(error) })
-        })
-    })
-}
 
 fs.readdir(input, function (err, files) {
     if (err) {
         console.log("There was an error reading files from the input folder");
     }
 
-    files.forEach(function (file) {
-        optimizeImage(file).then(
-            function (value) {
-                console.log(value);
-            },
-            function (error) {
-                console.log(error);
-            }
-        )
+    files.forEach(async function (file) {
+        let extension = path.extname(file); // extension of the image file
+        let baseFilename = path.basename(file, extension); // filename of the image minus the extension
+        let inputFile = `${input}${file}`;
+        let outputFile = `${output}${baseFilename}`;
+
+        // get the sizes for the image
+        const { width, height } = await sharp(inputFile).metadata();
+
+        // collect all the exif and iptc data from the image
+        let options = {
+            iptc: ['ObjectName', 'Caption'],
+            exif: ['ExposureTime', 'FNumber', 'ISO', 'DateTimeOriginal', 'ShutterSpeedValue', 'ApertureValue', 'Focal Length', 'WhiteBalance', 'LensModel'],
+            ifd0: ['Make', 'Model'],
+            mergeOutput: false
+        }
+        // get metadata
+        let metadata = await exifr.parse(inputFile, options);
+        // add the height and width from sharp
+        metadata = Object.assign({ "width": width, "height": height }, metadata)
+        let json = JSON.stringify(metadata);
+        fs.writeFileSync(`${outputFile}.json`, json);
     });
+    //getMetaData(file, outputPath);
+    //optimizeImage(file, outputPath);
 });
